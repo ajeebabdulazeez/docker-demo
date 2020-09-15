45e317afb781142609e97194832216f945edd646
AjeebAbdulAzeez <ajeeba01@gmail.com>
2020-09-14 22:49:48 -0400

    second commit

--- src/main/java/com/learn/dockerdemo/controller/TestController.java
+++ src/main/java/com/learn/dockerdemo/controller/TestController.java
@@ -1,8 +1,18 @@
 package com.learn.dockerdemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Ajeeb Abdul Azeez
  */
+@RestController
 public class TestController
 {
+
+	@GetMapping("/hello")
+	public String hello()
+	{
+		return "Hi from controller";
+	}
 }
